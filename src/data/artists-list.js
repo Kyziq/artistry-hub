1c6af1421658ee92cc59c0d0d788a6abaaefeea2
Haziq Khairi <ikhairulhaziq11@gmail.com>
2024-01-05 20:29:59 +0800

    feat: add artists list

---
+++ src/data/artists-list.js
@@ -0,0 +1,17 @@
+import JohannesVermeer from '../../assets/images/artists/JohannesVermeer.jpg';
+
+export const artists = [
+  {
+    name: 'Johannes Vermeer',
+    bio: 'Johannes Vermeer was a Dutch Baroque Period painter known for his masterly treatment and use of light in his work.',
+    birthYear: 1632,
+    deathYear: 1675,
+    nationality: 'Dutch',
+    notableWorks: [
+      'Girl with a Pearl Earring',
+      'The Milkmaid',
+      'View of Delft',
+    ],
+    imageUrl: JohannesVermeer,
+  },
+];
