cda7fe120ff23fdce7e236437e0a747c5b331d7a
Arif <4arif401@gmail.com>
2024-01-05 19:06:47 +0800

    ngetry tambah portrait

--- src/utils/constants/portraits-list.js
+++ src/utils/constants/portraits-list.js
@@ -4,4 +4,14 @@ export const potraitsList = [
     img: '/src/assets/portraits/LeonardoDaVinci/MonaLisa.jpg',
     artist: 'Leornardo Da Vinci',
   },
+  {
+    name: "Whistler's Mother",
+    img: '/src/assets/portraits/JamesAbbott/WhistlersMother.jpg',
+    artist: 'James Abbott McNeill Whistler',
+  },
+  {
+    name: "Whistler's Mother",
+    img: '/src/assets/portraits/JohannesVermeer/GirlwithAPearlEarring.jpg',
+    artist: 'Johannes Vermeer',
+  },
 ];
