c75eb07590959af7eff6582e09a5dd02e86ef106
Haziq Khairi <ikhairulhaziq11@gmail.com>
2024-01-06 13:37:44 +0800

    feat: add Navbar component and update App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,42 @@
-import { Tooltip } from '@nextui-org/react';
+import { Tooltip, Image } from '@nextui-org/react';
 import { categories } from './data/categories';
+import Navbar from './components/Navbar';
+
+const CategoryItem = ({ category }) => (
+  <Tooltip
+    key={category.name}
+    content={category.tooltip}
+    placement="top"
+    showArrow
+  >
+    <div className="flex flex-col items-center">
+      <div className="w-24 h-24 rounded-full overflow-hidden bg-white shadow-md">
+        <Image isZoomed alt={category.name} src={category.imageUrl} />
+      </div>
+    </div>
+  </Tooltip>
+);
 
 function App() {
   return (
-    <div className="flex flex-col justify-start items-start min-h-screen bg-gray-100 p-10">
-      <header className="text-2xl font-bold">ArtistryHub.</header>
-      <p className="text-gray-700">Explore our world of creative designs.</p>
+    <>
+      <Navbar />
 
-      <div className="mt-5">
-        <h2 className="text-xl font-bold">Categories</h2>
+      <div className="flex flex-col justify-start items-start min-h-screen p-6">
+        <header className="text-2xl font-bold">ArtistryHub.</header>
+        <p className="text-gray-700">Explore our world of creative designs.</p>
 
-        <div className="flex flex-wrap justify-start items-center gap-4 mt-4">
-          {categories.map((category) => (
-            <Tooltip
-              key={category.name}
-              content={category.tooltip}
-              placement="top"
-              showArrow
-            >
-              <div className="flex flex-col items-center">
-                <div className="w-24 h-24 rounded-full overflow-hidden bg-white shadow-md">
-                  <img
-                    src={category.imageUrl}
-                    alt={category.name}
-                    className="object-cover w-full h-full"
-                  />
-                </div>
-              </div>
-            </Tooltip>
-          ))}
+        <div className="mt-5">
+          <h2 className="text-xl font-bold">Categories</h2>
+
+          <div className="flex flex-wrap justify-start items-center gap-4 mt-4">
+            {categories.map((category) => (
+              <CategoryItem key={category.name} category={category} />
+            ))}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
