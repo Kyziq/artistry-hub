aad4e1cfdd936c58f2c73820a98f7b6d5a6ca073
Arif <4arif401@gmail.com>
2024-01-07 11:53:20 +0800

    tambah more arts pnya div layout (2 column baru)

--- src/App.jsx
+++ src/App.jsx
@@ -17,10 +17,15 @@ const shuffleArray = (array) => {
 
 function App() {
   // Combine data from landscapesList, paintingsList, and portraitsList
-  //const combinedList = [...landscapesList, ...paintingsList, ...portraitsList, ...abstractsList];
+  const combinedList = [
+    ...landscapesList,
+    ...paintingsList,
+    ...portraitsList,
+    ...abstractsList,
+  ];
 
   // Shuffle the combined list
-  //const shuffledList = shuffleArray(combinedList);
+  const shuffledList = shuffleArray(combinedList);
 
   // State to hold the new list
   const [newList, setNewList] = useState([]);
@@ -38,6 +43,13 @@ function App() {
 
   // Limit the number of items to show in the Top Picks section
   const limitedTopPicksList = newList.slice(0, 7);
+
+  // Limit the number of items to show in the More Arts section
+  const limitedMoreArtsList = shuffledList.slice(0, 6); // Change the number as needed
+
+  // Separate limitedMoreArtsList into two parts
+  const moreArtsContainer1List = limitedMoreArtsList.slice(0, 3);
+  const moreArtsContainer2List = limitedMoreArtsList.slice(3, 6);
 
   return (
     <div className="flex flex-col justify-start items-start min-h-screen bg-gray-100 p-10 overflow-hidden">
@@ -72,7 +84,7 @@ function App() {
 
         <div className="mt-5">
           <h2 className="text-xl font-bold">Top Picks</h2>
-          <br></br>
+          <br />
           <div className="top-picks-container flex overflow-x-auto">
             {limitedTopPicksList.map((item) => (
               <Tooltip
@@ -92,6 +104,58 @@ function App() {
                 </div>
               </Tooltip>
             ))}
+          </div>
+        </div>
+
+        <br />
+
+        <div className="mt-5">
+          <h2 className="text-xl font-bold">More Arts</h2>
+          <br />
+          <div className="flex">
+            <div className="more-arts-container-1 w-60 mr-2">
+              {moreArtsContainer1List.map((item) => (
+                <Tooltip
+                  key={item.name}
+                  content={'View ' + item.name}
+                  placement="bottom"
+                  showArrow
+                >
+                  <div className="flex flex-row flex-col items-center mb-1">
+                    <div className="w-50 rounded-lg bg-white shadow-md border border-gray-300">
+                      <img
+                        src={item.img}
+                        alt={item.name}
+                        className="object-cover w-full"
+                        style={{ height: '100%' }}
+                      />
+                    </div>
+                  </div>
+                </Tooltip>
+              ))}
+            </div>
+
+            <div className="more-arts-container-2 w-60 mr-2">
+              {moreArtsContainer2List.map((item) => (
+                <Tooltip
+                  key={item.name}
+                  content={'View ' + item.name}
+                  placement="bottom"
+                  showArrow
+                >
+                  <div className="flex flex-row flex-col items-center mb-1">
+                    <div className="w-50 rounded-lg bg-white shadow-md border border-gray-300">
+                      <img
+                        src={item.img}
+                        alt={item.name}
+                        className="object-cover w-full"
+                        style={{ height: '100%' }}
+                      />
+                    </div>
+                  </div>
+                </Tooltip>
+              ))}
+            </div>
           </div>
         </div>
       </div>
