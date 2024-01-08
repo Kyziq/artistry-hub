ca51d346fcf248c69d5a7befffd32020c9a84dc8
Arif <4arif401@gmail.com>
2024-01-09 00:03:01 +0800

    feat : error page and routed for now

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Test from './pages/Test';
 import Footer from './components/Footer';
+import ErrorPage from './pages/Error';
+
 function App() {
   return (
     <div className="flex flex-col justify-start items-start min-h-screen bg-gray-100">
@@ -11,8 +13,9 @@ function App() {
 
         <Routes>
           <Route exact path="/artistry-hub/" element={<Home />} />
-          <Route exact path="/test" element={<Test />} />
+          <Route exact path="/artistry-hub/test" element={<Test />} />
           {/* <Route component={GenericNotFound} /> */}
+          <Route path="*" element={<ErrorPage />} />
         </Routes>
       </Router>
       <Footer />
--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,8 +1,10 @@
+import bobotimg from '../assets/images/4errorPage/404image.png';
+
 const ErrorPage = () => {
   return (
-    <div className="flex flex-col justify-start items-start min-h-screen bg-gray-100 overflow-hidden p-10">
-      <div className="flex items-center justify-between w-full">
-        <div>
+    <div className="flex flex-col justify-start items-start min-h-screen bg-gray-100 overflow-hidden p-10 ml-20">
+      <div className="flex items-center justify-between w-full p-10 ml-20">
+        <div className="p-10 ml-20">
           <h1 className="text-4xl font-bold text-black-500 mb-10 mt-12">
             ArtistryHub.
           </h1>
@@ -14,7 +16,10 @@ const ErrorPage = () => {
             This page does not exist.
           </p>
         </div>
-        <div className="text-6xl p-10"> &#129402;</div>
+        <div className="text-6xl p-10 ml-20">
+          {' '}
+          <img src={bobotimg} alt="Bot Image" />{' '}
+        </div>
       </div>
     </div>
   );
