74b09b1adf8d3dc53558ac9224d1cf3eba3ae4c2
Haziq Khairi <ikhairulhaziq11@gmail.com>
2024-01-09 22:19:03 +0800

    feat: add link to explore featured artworks

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,23 @@
 import MoreArt from './MoreArt';
 import Categories from '../components/Categories';
+import { Link } from 'react-router-dom';
+import { FaArrowRight } from 'react-icons/fa'; // Importing an arrow icon
 
 const Home = () => {
   return (
     <div className="p-6">
-      <header className="text-2xl font-bold">ArtistryHub.</header>
-      <p className="text-gray-700">Explore our world of creative designs.</p>
+      <div className="flex justify-between items-center">
+        <header className="text-2xl font-bold">ArtistryHub.</header>
+        <Link
+          to="/artistry-hub/top-picks"
+          className="flex items-center text-lg font-medium text-blue-500 hover:text-blue-600 transition duration-300 ease-in-out"
+        >
+          Explore Featured Artworks <FaArrowRight className="ml-2" />
+        </Link>
+      </div>
+      <p className="text-gray-700 mt-2">
+        Explore our world of creative designs.
+      </p>
 
       <Categories />
 
