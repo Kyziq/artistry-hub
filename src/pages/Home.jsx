cca9732fb7db3ecfdb4b96de9899d8480e5203cb
Haziq Khairi <ikhairulhaziq11@gmail.com>
2024-01-09 21:53:04 +0800

    feat: add Categories component and refactor Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,5 @@
-import { Tooltip, Image } from '@nextui-org/react';
-import { categories } from '../data/categories';
 import MoreArt from './MoreArt';
-
-const CategoryItem = ({ category }) => (
-  <Tooltip content={category.tooltip} placement="top" showArrow>
-    <div className="flex flex-col items-center">
-      <Image
-        src={category.imageUrl}
-        alt={category.name}
-        isZoomed
-        className="w-24 h-24"
-      />
-      <span className="mt-1 text-sm">{category.name}</span>
-    </div>
-  </Tooltip>
-);
+import Categories from '../components/Categories';
 
 const Home = () => {
   return (
@@ -22,19 +7,9 @@ const Home = () => {
       <header className="text-2xl font-bold">ArtistryHub.</header>
       <p className="text-gray-700">Explore our world of creative designs.</p>
 
-      <div className="mt-5">
-        <h2 className="text-2xl font-bold">Categories</h2>
+      <Categories />
 
-        <div className="flex flex-wrap justify-start items-center gap-4 mt-4">
-          {categories.map((category) => (
-            <CategoryItem key={category.name} category={category} />
-          ))}
-        </div>
-      </div>
-
-      <div className="mt-5">
-        <MoreArt />
-      </div>
+      <MoreArt />
     </div>
   );
 };
